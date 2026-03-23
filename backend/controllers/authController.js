9a8f4a7ceb03a83f0334bf29fd443d18c59bb91c
NAVENKS <ksnaven@gmail.com>
2026-03-23 15:47:49 +0530

    replace bcrypt with bcryptjs for linux compatibility

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { findUserByEmail } = require('../models/queries');
 require('dotenv').config();
